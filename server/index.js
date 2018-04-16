33755c4ab4073a1b5e44d7fbda7aa2db244f8f68
suchya1 <35808138+suchya1@users.noreply.github.com>
2018-04-16 19:54:33 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const path=require('path');
-var app = express()
-
-const servername = "localhost";
-const port = 8080;
+const bodyParser=require('body-parser');
 
 const simple = require('./simpleController');
 const game = require('./game/controller');
 
 
+var app = express()
+
+const servername = "localhost";
+const port = 8080;
+
+
+
+
 app
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
     .use('/',(req,res,next)=>{
         res.header("Access-Control-Allow-Origin","*")
         res.header("Access-Control-Allow-Headers","*")
