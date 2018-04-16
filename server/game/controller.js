ad1db4fd1a39d6628e3c2dcf3402caefa0d6fb24
suchya1 <35808138+suchya1@users.noreply.github.com>
2018-04-16 19:54:59 -0400

    Update controller.js

--- server/game/controller.js
+++ server/game/controller.js
@@ -7,7 +7,12 @@ var app = express.Router();
 var game = new Game();
 
 module.exports = app
-    .get('/quotes', (req, res) => res.send(game.GetQuotes())) 
+    .get('/quotes', (req, res) => res.send(game.GetQuotes(req.query.playerId))) 
     .get('/state',(req,res) => res.send(game))
     .post('/picture',(req,res) => res.send(game.FlipPicture()))
-    
+    .post('/quotes',(req,res) =>{
+       // console.log(req.body) 
+        game.SubmitQuote(req.body.Text, req.body.PlayerId)
+        res.send({success: true});
+
+    })
